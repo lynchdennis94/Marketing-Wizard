34d37180593d5df7d7831485d44813ac183dcaf1
lynchdennis94 <lynch.dennis94@gmail.com>
2015-09-10 22:11:39 -0400

    v1.7a: Added attachments and addresses layout to Preview Window and beginning of MessageObject framework; Reorganized objects into new folders

---
+++ MarketWizard/MarketWizard/Objects/MessageObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketWizard.Objects
+{
+    class MessageObject
+    {
+        private string subject;
+        private string body;
+        private string attachmentLocation;
+        private string addressesLocation;
+    }
+}
