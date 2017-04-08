c38f98158eda87b9882ac33afec689afed746f5c
dlynch35 <lynch.dennis94@gmail.com>
2017-04-08 02:45:55 -0400

    Added message interface and basic message object

---
+++ MarketWizard/MarketWizard/Models/EmailMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketWizard.Models {
+    class EmailMessage : IMessage{
+        private List<String> recepients;
+        private String subject;
+        private String body;
+
+        public EmailMessage(String subject, String body) {
+            this.subject = subject;
+            this.body = body;
+        }
+
+        public EmailMessage(String recepient, String subject, String body) : this(subject, body) {
+            List<String> recepientList = new List<String>();
+            recepientList.Add(recepient);
+            this.recepients = recepientList;
+        }
+
+        public EmailMessage(List<String> recepients, String subject, String body) : this(subject, body) {
+            this.recepients = recepients;
+        }
+
+        public List<String> getRecepients() {
+            return recepients;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+    }
+}
---
+++ MarketWizard/MarketWizard/Models/IMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketWizard.Models {
+    interface IMessage {
+        public List<String> getRecepients();
+        public String getBody();
+    }
+}
