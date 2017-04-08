a2c404b2ce54917999ae797ef931992be42781cd
dlynch35 <lynch.dennis94@gmail.com>
2017-04-08 02:44:55 -0400

    Moved v1 of Marketing Wizard to old folders for reference until v2 done

---
+++ MarketWizard/MarketWizard/Objects - v1/EmailObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+
+namespace MarketWizard
+{
+    public class EmailObject
+    {
+        private String from;
+        private String to;
+        private String subject;
+        private String body;
+
+
+        private MailMessage mailMessage;
+
+        private const int SECONDS_TO_HALT = 10;
+
+        public EmailObject(String from, String to, String subject, String bodyFilepath, String attachmentFilepath)
+        {
+            this.from = from;
+            this.to = to;
+            this.subject = subject;
+            this.body = System.IO.File.ReadAllText(@bodyFilepath);
+            this.mailMessage = new MailMessage(from, to, subject, this.body);
+
+            List<Attachment> attachments = getAttachments(attachmentFilepath);
+            foreach (Attachment attachment in attachments) {
+                this.mailMessage.Attachments.Add(attachment);
+            }
+        }
+
+        private List<Attachment> getAttachments(String attachmentFilepath) {
+            String[] separators = { ";" };
+            String[] attachments = @attachmentFilepath.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            List<Attachment> attachmentList = new List<Attachment>();
+            foreach (String newAttachment in attachments) {
+                attachmentList.Add(new Attachment(newAttachment));
+            }
+
+            return attachmentList;
+        }
+
+        public String getFrom() {
+            return this.from;
+        }
+
+        public String getTo() {
+            return this.to;
+        }
+
+        public String getSubject() {
+            return this.subject;
+        }
+
+        public String getBody() {
+            return this.body;
+        }
+
+        public MailMessage getMessage() {
+            return this.mailMessage;
+        }
+    }
+}
---
+++ MarketWizard/MarketWizard/Windows - v1/PreviewWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarketWizard
+{
+    /// <summary>
+    /// Interaction logic for PreviewWindow.xaml
+    /// </summary>
+    public partial class PreviewWindow : Window
+    {
+        private SMTPObject previewEmailObj;
+        public PreviewWindow(SMTPObject emailToPreview)
+        {
+            previewEmailObj = emailToPreview;
+            InitializeComponent();
+            loadPreview();
+
+            // DEBUG ListBox
+            addressListBox.SelectionMode = SelectionMode.Multiple;
+            for (int test = 500600; test < 500750; test++)
+            {
+                addressListBox.Items.Add(test.ToString());
+            }
+            addressListBox.SelectAll();
+        }
+
+        private void loadPreview()
+        {
+            // Get the information from the preview email object, and populate the window
+            subjectTextBox.Text = previewEmailObj.getSubject();
+            bodyTextBlock.Text = previewEmailObj.getBody();
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Just back out of this window, we aren't sending or saving changes
+            this.Close();
+        }
+    }
+}
